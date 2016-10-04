af1c11d38451b2c9eefc21a391e565c07b217599
oyamasiphula <oyama@projectcodex.co>
2016-10-04 23:59:58 +0200

    update

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var github = require('octonode');
+var express = require('express'),
+    express_handlebars = require('express-handlebars'),
+    github = require('octonode');
 
+const app = express();
 const client = github.client();
 
 
-var filesForUser = function(username, repoName, cb){
+app.engine('handlebars', express_handlebars({defaultLayout: 'main'}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+var specificUserFilePool = function(username, repoName, cb){
     client
         .get('/repos/' + username + '/' + repoName + '/contents', function(err, results, data){
 
             var fileNames = data.map(function(entry){
                 return entry.name;
             });
-
             //make sure it's a true async call
             process.nextTick(function(){
                 cb(err, fileNames);
@@ -37,15 +45,42 @@ dynamically_typed.js
 type_errors.js
 empty_variables.js
 */
+var repofiles = []
+var userData = {};
 
-filesForUser('MsEmma', '53functions', function(err, files){
-    console.log( 'MsEmma : ' + files.length);
+specificUserFilePool('MsEmma', '53functions', function(err, files){
+    console.log( 'MsEmma : ' + repofiles.push(files));
+    console.log(repofiles);
+
+
+  for (var i = 0; i < files.length; ++i)
+    userData[i] = files[i];
+    console.log(userData);
+    return userData;
 });
 
-filesForUser('avermeulen', '53functions', function(err, files){
+specificUserFilePool('avermeulen', '53functions', function(err, files){
     console.log( 'avermeulen : ' + files.length);
 });
 
-filesForUser('Oyamasiphula', '53functions', function(err, files){
+specificUserFilePool('Oyamasiphula', '53functions', function(err, files){
     console.log('Oyamasiphula : ' + files.length);
 });
+
+
+app.get('/', function(req, res){
+	res.render('home');
+})
+app.get('about',function(req,res){
+  res.render('about');
+
+})
+// <portSetup>port delcaration
+var port = process.env.port || 2000
+// </portSetup>
+
+// <serveCodeBlocksRun>Lets configure our localhost server's port
+app.listen(port,function(){
+console.log('app is listening on' + port);
+});
+// </serveCodeBlocksRun>

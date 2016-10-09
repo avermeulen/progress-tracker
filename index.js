5bf8d1100e7242d34ab71848e0c88fe5e1641b1c
oyamasiphula <oyama@projectcodex.co>
2016-10-09 12:38:29 +0200

    managed :-) to get the data back based on a specific user(name)|| content

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express'),
     express_handlebars = require('express-handlebars'),
     github = require('octonode'),
     // Load the full build.
-     _ = require('lodash'),
-     // Load the core build.
-     _ = require('lodash/core'),
+    _ = require('lodash'),
+    // Load the core build.
+    _ = require('lodash/core'),
     // Load the FP build for immutable auto-curried iteratee-first data-last methods.
     fp = require('lodash/fp'),
     // Load method categories.
@@ -12,45 +12,41 @@ var express = require('express'),
     object = require('lodash/fp/object');
 
 const app = express(),
-      client = github.client();
+    client = github.client();
 
 
-
-
-
-app.engine('handlebars', express_handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', express_handlebars({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-var specificUserFilePool = function(ghUserId, repository_name, cb){
-
+var specificUserFilePool = function(ghUserId, repository_name, cb) {
     client
-        .get('/repos/' + ghUserId + '/' + repository_name + '/contents', function(err, results, data){
+        .get('/repos/' + ghUserId + '/' + repository_name + '/contents', function(err, results, data) {
 
-        var holdFileNames = data.map(function(entry){
-            var holdAllFiles = entry.name;
-              return holdAllFiles;
-        })
-        // console.log(holdFileNames.length);
-        var file_No = holdFileNames.length
-        // gather user specifics but without the get user module(Plugin)
-        var detailedUserContentObj = {
-             ghUserId :  ghUserId,
-             repository_name : repository_name,
-             holdFileNames : file_No
-        };
+            var holdFileNames = data.map(function(entry) {
+                var holdAllFiles = entry.name;
+                return holdAllFiles;
+            });
+            var numberOfFiles = holdFileNames.length;
+            // gather user specifics but without the get user module(Plugin)
+            var detailedUserContentObj = {
+                ghUserId: ghUserId,
+                repository_name: repository_name,
+                file_List: holdFileNames,
+                fileNo: numberOfFiles
+            };
             //make sure it's a true async call
-            process.nextTick(function(){
+            process.nextTick(function() {
                 cb(err, detailedUserContentObj);
-            })
-          });
+            });
+        })
 
 };
-
-
 // list of bootcamp users github accounts
 //SinethembaDlova
 
@@ -68,42 +64,25 @@ dynamically_typed.js
 type_errors.js
 empty_variables.js
 */
-var trackedUser = 'MsEmma';
-app.get('/', function(req, res){
 
-  specificUserFilePool('MsEmma', '53functions', function(err, detailedUserContent){
-    console.log(detailedUserContent);
-
-      res.render('usersDataPresentation',{
-        filesNameResult : detailedUserContent
-    })
-  });
-});
-specificUserFilePool('avermeulen', '53functions', function(err, detailedUserContent){
-
-    // console.log( 'avermeulen : ' + files.length);
-    console.log(detailedUserContent);
-
-});
-
-specificUserFilePool('Oyamasiphula', '53functions', function(err, detailedUserContent){
-    // console.log('Oyamasiphula : ' + files.length);
+app.get('/repos/:ghUserId/:repository_name/contents', function(req, res) {
+    var ghUserId = req.params.ghUserId;
+    var repository_name = req.params.repository_name;
+    specificUserFilePool(ghUserId, repository_name, function(err, detailedUserContent) {
+        console.log(detailedUserContent);
+        res.render('usersDataPresentation', {
+            filesNameResult: detailedUserContent
+        })
+    });
 });
 
 
-app.get('/', function(req, res){
-	res.render('home');
-})
-app.get('about',function(req,res){
-  res.render('about');
-
-})
 // <portSetup>port delcaration
 var port = process.env.port || 2001
-// </portSetup>
+    // </portSetup>
 
 // <serveCodeBlocksRun>Lets configure our localhost server's port
-app.listen(port,function(){
-console.log('app is listening on' + port);
+app.listen(port, function() {
+    console.log('app is listening on' + port);
 });
 // </serveCodeBlocksRun>

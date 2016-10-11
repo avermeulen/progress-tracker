de4c6cd7a8cb76b88e84d224b188c799e4283d37
oyamasiphula <oyama@projectcodex.co>
2016-10-11 19:16:25 +0200

    :-)  finally I managed to create file list Object

--- routes/trackUsers.js
+++ routes/trackUsers.js
@@ -1,23 +1,23 @@
 const github = require('octonode'),
-      client = github.client();
+    client = github.client();
 
 var specificUserFilePool = function(user_name, repository_name, cb) {
     client
         .get('/repos/' + user_name + '/' + repository_name + '/contents', function(err, results, data) {
-          // var handleRequest = function(req, res) {
-          //     try {
-          //       res.writeHead(200);
-          //       res1.end('Hello, World!\n');
-          //     } catch(e) {
-          //       res.writeHead(200);
-          //       res.end('Boo');
-          //     }
-          // };
-          // var server = require('http').createServer(handleRequest);
-          // process.on('uncaughtException', function(ex) {
-          //           console.log('Something went wrong')
-          //     // do something with exception
-          // });
+            // var handleRequest = function(req, res) {
+            //     try {
+            //       res.writeHead(200);
+            //       res1.end('Hello, World!\n');
+            //     } catch(e) {
+            //       res.writeHead(200);
+            //       res.end('Boo');
+            //     }
+            // };
+            // var server = require('http').createServer(handleRequest);
+            // process.on('uncaughtException', function(ex) {
+            //           console.log('Something went wrong')
+            //     // do something with exception
+            // });
 
             var fileNames = data.map(function(entry) {
                 var holdAllFiles = entry.name;
@@ -43,12 +43,30 @@ exports.getUserRepoContent = function(req, res) {
     var user_name = req.params.user_name;
     var repository_name = req.params.repository_name;
 
-        specificUserFilePool(user_name, repository_name, function(err, detailedUserContent) {
-            console.log(detailedUserContent.file_List);
-            var files = detailedUserContent.file_List
+    specificUserFilePool(user_name, repository_name, function(err, detailedUserContent) {
+        console.log(detailedUserContent.file_List);
+        var files = detailedUserContent.file_List
 
-            res.render('usersDataPresentation', {
-                filesNameResult: detailedUserContent
+        res.render('usersDataPresentation', {
+            filesNameResult: detailedUserContent
+        })
+    });
+};
+
+exports.userFileRepoCheck = function(req, res) {
+    var user_name = req.params.user_name;
+    var repository_name = req.params.repository_name;
+
+    specificUserFilePool(user_name, repository_name, function(err, detailedUserContent) {
+        var files = detailedUserContent.file_List;
+        var fileList = [];
+
+        files.map(function(file) {
+            fileList.push(currentExistingFileNames = {
+                file_name: file
             })
+            console.log(currentExistingFileNames);
+            return currentExistingFileNames.file_name;
+        })
     });
 };

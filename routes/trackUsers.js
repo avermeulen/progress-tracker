960e9015aed633faf81bca2ce9e5246cc24237a3
oyamasiphula <oyama@projectcodex.co>
2016-10-11 13:48:39 +0200

    renamed variables for simplicity (readable code)

--- routes/trackUsers.js
+++ routes/trackUsers.js
@@ -1,48 +1,37 @@
 const github = require('octonode'),
-    client = github.client();
+      client = github.client();
 
-var specificUserFilePool = function(ghUserId, repository_name, cb) {
+var specificUserFilePool = function(user_name, repository_name, cb) {
     client
-        .get('/repos/' + ghUserId + '/' + repository_name + '/contents', function(err, results, data) {
+        .get('/repos/' + user_name + '/' + repository_name + '/contents', function(err, results, data) {
+          // var handleRequest = function(req, res) {
+          //     try {
+          //       res.writeHead(200);
+          //       res1.end('Hello, World!\n');
+          //     } catch(e) {
+          //       res.writeHead(200);
+          //       res.end('Boo');
+          //     }
+          // };
+          // var server = require('http').createServer(handleRequest);
+          // process.on('uncaughtException', function(ex) {
+          //           console.log('Something went wrong')
+          //     // do something with exception
+          // });
 
-            var holdFileNames = data.map(function(entry) {
+            var fileNames = data.map(function(entry) {
                 var holdAllFiles = entry.name;
+                // holdAllFiles
                 return holdAllFiles;
             });
-            var numberOfFiles = holdFileNames.length;
+            var numberOfFiles = fileNames.length;
             // gather user specifics but without the get user module(Plugin)
             var detailedUserContentObj = {
-                ghUserId: ghUserId,
+                user_name: user_name,
                 repository_name: repository_name,
-                file_List: holdFileNames,
+                file_List: fileNames,
                 fileNo: numberOfFiles
             };
-
-            function arr_diff(userFiles, expectedUserFiles) {
-
-                var existingFiles = [],
-                    diff = [];
-
-                for (var i = 0; i < userFiles.length; i++) {
-                    existingFiles[userFiles[i]] = true;
-                }
-
-                for (var i = 0; i < expectedUserFiles.length; i++) {
-                    if (existingFiles[expectedUserFiles[i]]) {
-                        delete existingFiles[expectedUserFiles[i]];
-                    } else {
-                        existingFiles[expectedUserFiles[i]] = true;
-                    }
-                }
-
-                for (var k in existingFiles) {
-                    diff.push(k);
-                }
-                console.log(diff)
-                return diff;
-            };
-
-            arr_diff();
             //make sure it's a true async call
             process.nextTick(function() {
                 cb(err, detailedUserContentObj);
@@ -51,33 +40,15 @@ var specificUserFilePool = function(ghUserId, repository_name, cb) {
 };
 
 exports.getUserRepoContent = function(req, res) {
-    var ghUserId = req.params.ghUserId;
+    var user_name = req.params.user_name;
     var repository_name = req.params.repository_name;
 
-    specificUserFilePool(ghUserId, repository_name, function(err, detailedUserContent) {
-        function arr_diff(userFiles, expectedUserFiles) {
+        specificUserFilePool(user_name, repository_name, function(err, detailedUserContent) {
+            console.log(detailedUserContent.file_List);
+            var files = detailedUserContent.file_List
 
-            var existingFiles = [],trackDiff = [];
-
-            for (var i = 0; i < userFiles.length; i++) {
-                existingFiles[userFiles[i]] = true;
-            };
-            for (var i = 0; i < expectedUserFiles.length; i++) {
-                if (existingFiles[expectedUserFiles[i]]) {
-                    delete existingFiles[expectedUserFiles[i]];
-                } else {
-                    existingFiles[expectedUserFiles[i]] = true;
-                }
-            };
-            for (var file_names in existingFiles) {
-                trackDiff.push(file_names);
-            }
-            console.log(trackDiff);
-            return trackDiff;
-        };
-        // console.log(detailedUserContent);
-        res.render('usersDataPresentation', {
-            filesNameResult: detailedUserContent
-        })
+            res.render('usersDataPresentation', {
+                filesNameResult: detailedUserContent
+            })
     });
 };

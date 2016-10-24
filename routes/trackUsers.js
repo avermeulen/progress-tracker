d2035a7f1f4b9e5ace51805329a69d7b4455d92f
oyamasiphula <oyama@projectcodex.co>
2016-10-24 21:56:35 +0200

    removed unecessary code

--- routes/trackUsers.js
+++ routes/trackUsers.js
@@ -5,22 +5,21 @@ const github = require('octonode'),
     client = github.client();
 
 var specificUserFilePool = function(user_name, repository_name, cb) {
+
     client
         .get('/repos/' + user_name + '/' + repository_name + '/contents', function(err, results, data) {
-            // var handleRequest = function(req, res) {
-            //     try {
-            //       res.writeHead(200);
-            //       res1.end('Hello, World!\n');
-            //     } catch(e) {
-            //       res.writeHead(200);
-            //       res.end('Boo');
-            //     }
-            // };
-            // var server = require('http').createServer(handleRequest);
-            // process.on('uncaughtException', function(ex) {
-            //           console.log('Something went wrong')
-            //     // do something with exception
-            // });
+
+            // The app crashes when it reaches this part
+
+            if (data === "undefined") {
+              console.log('Fix me , and make it clear that you"ve fixed me on github')
+            } else {
+
+              function  findErrorSync(req , res){
+                    res.send('Fix me')
+              };
+              findErrorSync();
+            }
 
             var fileNames = data.map(function(entry) {
                 var holdAllFiles = entry.name;
@@ -46,7 +45,7 @@ exports.getUserRepoContent = function(req, res) {
     var repository_name = req.params.repository_name;
 
     specificUserFilePool(user_name, repository_name, function(err, detailedUserContent) {
-        var files = detailedUserContent.file_List
+        var files = detailedUserContent.file_List;
 
         res.render('usersDataPresentation', {
             filesNameResult: detailedUserContent
@@ -84,7 +83,6 @@ exports.userFileRepoCheck = function(req, res) {
                 exist: _.includes(files, expectedFile)
             };
         });
-
 
         var details = {
             username: user_name,
